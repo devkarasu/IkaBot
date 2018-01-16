910e883b4b8e4f7e7740aac92fb1b4231755c239
Mashiro <devkarasu@gmail.com>
2018-01-17 05:27:18 +0900

    ナワバリをうろつくようにした

--- Ikabot.java
+++ Ikabot.java
@@ -1,6 +1,7 @@
 package TeamIka;
 import robocode.*;
 import TeamIka.Territory;
+import java.awt.geom.Point2D;
 //import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
@@ -12,6 +13,17 @@ public class Ikabot extends AdvancedRobot
 {
 	Territory nawabari = new Territory(getX(), getY());
 	public void run() {
-		
+		while(true){
+			moveToPoint(nawabari.getNextPoint());
+		}
+	}
+
+	private void moveToPoint(Point2D.Double point) {
+		double dx = point.getX() - getX();
+		double dy = point.getY() - getY();
+		double angle = Math.toDegrees(Math.atan2(dx, dy)) - getHeading();
+	
+		turnRight(angle);
+		ahead(Math.sqrt(dx * dx + dy * dy));
 	}
 }
