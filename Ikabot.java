87b121b0558c252bdb64cadafcef3a53cd59dc41
Mashiro <devkarasu@gmail.com>
2018-02-16 16:44:19 +0900

    Javadocコメントの追加

--- Ikabot.java
+++ Ikabot.java
@@ -9,9 +9,25 @@ import java.awt.Color;
 
 /**
  * Ikabot - a robot by Kazuma Wada
+ * 創成工学実験で制作を行ったロボットです。
+ * ナワバリを持ち複雑な移動を行うことを特徴とします。
+ * @author 和田 一真
  */
 public class Ikabot extends AdvancedRobot
 {
+	/**
+	 * ロボットのメインメソッドです。
+	 * 次のような動作を行います。
+	 * <ul>
+	 * <li>Territoryクラスを使って新しいナワバリを生成します。</li>
+	 * <li>大砲とレーダーと車体が独立して動くよう設定します</li>
+	 * <li>ロボットのカラーを設定します</li>
+	 * <li>レーダーの回転が止まっていれば回し始めます。</li>
+	 * <li>ナワバリの頂点を順番に取り出し移動します。</li>
+	 * <li>40%の確率でゲームフィールド内にランダムに座標点を設定し移動します。</li>
+	 * </ul>
+	 * @see Territory
+	 */
 	public void run() {
 		Territory nawabari = new Territory(getX(), getY(), 20);
 		setAdjustGunForRobotTurn(true);
@@ -37,6 +53,11 @@ public class Ikabot extends AdvancedRobot
 		}
 	}
 
+	/**
+	 * 指定した座標点までロボットを移動させます。
+	 * @param point 目標座標
+	 * @see awt.geom.Point2D.Double
+	 */
 	private void goTo(Point2D.Double point) {
 		double x = point.x;
 		double y = point.y;
@@ -45,6 +66,17 @@ public class Ikabot extends AdvancedRobot
 		ahead(Math.hypot(x, y) * Math.cos(a));
 	}
 
+	/**
+	 * レーダーで他ロボットを発見した時にこのメソッドは呼び出されます。
+	 * 次のような動作を行います。
+	 * <ul>
+	 * <li>発見したロボットの方向にレーダーを向かせ続けます。</li>
+	 * <li>線形予測を用いた標的合わせを行い、発見したロボットを攻撃します。</li>
+	 * <li>このロボットが発見したロボットの方向にまっすぐ向かおうとした時、軌道を右に逸します。</li>
+	 * </ul>
+	 * @param e ゲームによって設定されるイベント
+	 * @see ScannedRobotEvent
+	 */
 	public void onScannedRobot(ScannedRobotEvent e){
 		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
 		double radar = absoluteBearing - getRadarHeadingRadians();
@@ -65,6 +97,10 @@ public class Ikabot extends AdvancedRobot
 			setTurnRight(100);
 	}
 
+	/**
+	 * このロボットが壁にぶつかった時に壁から離れます。
+	 * @param e ゲームによって設定されるイベント
+	 */
 	public void onHitWall(HitWallEvent e){
 		back(50);
 		ahead(30);
--- Territory.java
+++ Territory.java
@@ -5,16 +5,37 @@ import java.util.Iterator;
 import java.awt.geom.Point2D;
 
 /**
-* Territoryは、ロボットのナワバリに関する機能を提供するクラスです。
-* @author 和田一真
-*/
+ * Territoryは、ロボットのナワバリに関する機能を提供するクラスです。
+ * @author 和田 一真
+ */
 
 public class Territory{
+    /**
+     * ナワバリの中心点。
+     * この点を中心にナワバリを構成する頂点が決定されます。
+     */
     private Point2D.Double center;
+    /**
+     * ナワバリを構成する頂点座標のリスト。
+     */
     private ArrayList<Point2D.Double> points;
+    /**
+     * ナワバリを構成する頂点を順に辿るためのイテレータ。
+     */
     private Iterator<Point2D.Double> it;
+    /**
+     * ナワバリの頂点数。
+     */
     private int NUM;
 
+    /**
+     * 新しいナワバリを頂点数します。
+     * @param x 生成するナワバリの中心となるX座標
+     * @param y 生成するナワバリの中心となるy座標
+     * @param n 生成するナワバリの頂点数
+     * @see center
+     * @see points
+     */
     public Territory(double x, double y, int n){
         center = new Point2D.Double(x, y);
         NUM = n;
@@ -29,10 +50,20 @@ public class Territory{
         it = points.iterator();
     }
 
+    /**
+     * ナワバリの頂点数を戻します。
+     * @return ナワバリの頂点数
+     * @see NUM
+     */
     public int getNum(){
         return NUM;
     }
 
+    /**
+     * ナワバリの頂点座標を順番に戻します。
+     * 最後の頂点を戻した後は、一番最初に戻ります。
+     * @return ナワバリを構成する頂点のうち一つの座標
+     */
     public Point2D.Double getNextPoint(){
         if(it.hasNext()){
             return it.next();
